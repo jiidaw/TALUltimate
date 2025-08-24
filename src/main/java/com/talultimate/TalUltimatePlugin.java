d2bdd4b8b20e1ce6acaacd8595932fc4003cc47d
jiidaw <102522936+jiidaw@users.noreply.github.com>
2025-08-24 14:32:01 -0700

    Update TalUltimatePlugin.java

--- src/main/java/com/talultimate/TalUltimatePlugin.java
+++ src/main/java/com/talultimate/TalUltimatePlugin.java
@@ -1,76 +1,42 @@
-ppackage com.talultimate;
+package com.talultimate;
 
+import com.talultimate.features.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TalUltimatePlugin extends JavaPlugin {
-    // ...
-}
-ackage com.talultimate;
 
-import com.talultimate.features.WarpCommand;
-import com.talultimate.features.SetTitleCommand;
-import com.talultimate.features.ShopAdminCommand;
-import com.talultimate.listeners.LevelRewardListener;
-
-
-public class TalUltimatePlugin extends JavaPlugin {
-
-    public static EconomyManager ECON;
-    public static TitleManager TITLES;
-    public static HudManager HUD;
+    // 전역 매니저/싱글턴 (필요한 것만)
     public static WarpManager WARPS;
-    public static LandManager LAND;
-    public static TpaHomeManager TPAHOME;
     public static GachaManager GACHA;
-    public static ShopManager SHOP;
+    public static TitleManager TITLES; // 이미 있다면 기존 구현 사용
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
-        ECON = new EconomyManager(this);
-        TITLES = new TitleManager(this);
-        HUD = new HudManager(this);
+        // 매니저 생성
         WARPS = new WarpManager(this);
-        LAND = new LandManager(this);
-        TPAHOME = new TpaHomeManager();
         GACHA = new GachaManager(this);
-        SHOP = new ShopManager(this);
+        if (TITLES == null) TITLES = new TitleManager(this); // 이미 있으면 주석처리
 
-        register(new JoinQuitListener(this));
-        register(new AdvancementListener(this));
-        register(new LevelRewardListener(this));
-        register(new LandProtectListener());
+        // 리스너 등록
+        getServer().getPluginManager().registerEvents(new AdvancementListener(this), this);
+        getServer().getPluginManager().registerEvents(new LevelRewardListener(), this); // 무인자 생성자 기준
 
+        // 커맨드 등록
         getCommand("menu").setExecutor(new UiMenuCommand(this));
-        getCommand("money").setExecutor(new MoneyCommand(this));
-        getCommand("deposit").setExecutor(new MoneyCommand(this));
-        getCommand("withdraw").setExecutor(new MoneyCommand(this));
-        getCommand("sell").setExecutor(new SellCommand(this));
+        getCommand("gachaitem").setExecutor(new GachaCommand(this, false));
+        getCommand("gachatitle").setExecutor(new GachaCommand(this, true));
+        getCommand("settitle").setExecutor(new SetTitleCommand());           // 무인자
+        getCommand("shopadmin").setExecutor(new ShopAdminCommand(this));     // plugin 필요
         getCommand("광장").setExecutor(new WarpCommand(this, "plaza", "tal.warp.plaza"));
         getCommand("결투장").setExecutor(new WarpCommand(this, "arena", "tal.warp.arena"));
-        getCommand("tpa").setExecutor(TPAHOME);
-        getCommand("tpaccept").setExecutor(TPAHOME);
-        getCommand("tpdeny").setExecutor(TPAHOME);
-        getCommand("sethome").setExecutor(TPAHOME);
-        getCommand("home").setExecutor(TPAHOME);
-        getCommand("delhome").setExecutor(TPAHOME);
-        getCommand("addtitle").setExecutor(new TitleAdminCommand(this, true));
-        getCommand("removetitle").setExecutor(new TitleAdminCommand(this, false));
-        getCommand("settitle").setExecutor(new SetTitleCommand(this));
-        getCommand("shopadmin").setExecutor(new ShopAdminCommand(this));
-        getCommand("gachatitle").setExecutor(new GachaCommand(this, true));
-        getCommand("gachaitem").setExecutor(new GachaCommand(this, false));
 
-        HUD.start();
-        getLogger().info("TALUltimate loaded (full pack).");
+        getLogger().info("TALUltimate loaded!");
     }
 
     @Override
     public void onDisable() {
-        if (HUD != null) HUD.stop();
         getLogger().info("TALUltimate disabled.");
     }
-
-    private void register(Listener l) { Bukkit.getPluginManager().registerEvents(l, this); }
 }
